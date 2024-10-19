2510615883c1fdfee90e1ac7070fb85d0ab72a23
azkazufarh <azkazhanif@gmail.com>
2024-10-19 03:15:55 +0000

    UI Login Register

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [message, setMessage] = useState('');
+
+  const onSubmit = (data) => console.log(data)
+  return (
+    <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
+      <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-xl font-semibold mb-4">Please Login</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='mb-4'>
+            <label htmlFor="email" className='block text-gray-700 text-sm font-bold mb-2'>Email</label>
+            <input {...register("email", { required: true })} type="email" name='email' id='email' className='shadow apperance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow' placeholder='Email Address' />
+          </div>
+          <div className='mb-4'>
+            <label htmlFor="password" className='block text-gray-700 text-sm font-bold mb-2'>Password</label>
+            <input {...register("passsword", { required: true })} type="password" name='password' id='password' className='shadow apperance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow' placeholder='*******' />
+          </div>
+          {
+            message && <p className="text-red-500 text-xs italice mb-3">{message}</p>
+          }
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none">Login</button>
+        </form>
+        <p className="align-baseline font-medium mt-4 text-sm">Haven't an account? Please <Link to='/register' className='text-blue-500'>register</Link></p>
+        <div className='mt-4'>
+          <button className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
+            <FaGoogle className='mr-2' />
+            Sign in with Google
+          </button>
+        </div>
+
+        <p className='mt-5 text-center text-gray-500 text-xs'>@2025 Book Store. All rights reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/home/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/orders',
+                element: <h1>Orders</h1>
+            },
+            {
+                path: '/About',
+                element: <h1>About</h1>
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            }
+        ]
+    }
+])
+
+export default router;
